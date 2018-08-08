bfa77b287753252b06e79a0f1b3e0fcbc4682bc7
Gopal Kumar <gk55gupta@gmail.com>
2018-08-08 14:09:04 +0530

    added staff component with input fields and a service in admin.service to get centers

--- src/pages/mis/admin/staff/staff.component.ts
+++ src/pages/mis/admin/staff/staff.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../../../../providers/admin/admin.service';
 
 @Component({
-    selector: 'app-staff',
-    templateUrl: './staff.component.html'
+  selector: 'app-staff',
+  templateUrl: './staff.component.html'
 })
-
 export class AppStaffComponent implements OnInit {
-    constructor() {}
-    loading = true;
+  constructor(private adminService: AdminService) {}
+  loading = true;
+  centers: any;
+  selectedCenter: any = 'all';
+  staffType: object[] = [
+    { name: 'CxO', value: 'CxO' },
+    { name: 'Management', value: 'Management' },
+    { name: 'Teacher', value: 'Teacher' },
+    { name: 'Staff', value: 'Staff' }
+  ];
+  search: string;
+  selectedStaffStatus: any = 'true';
+  selectedStaffType: any = 'all';
+  viewPanel = false;
 
-    ngOnInit() {
+  ngOnInit() {
+    // setting centers response from backend
+    this.adminService.getCenter().subscribe(response => {
+      this.centers = response;
+      this.loading = false;
+    });
+  }
 
-    }
+  searchStaff() {
+    console.log(this.search);
+  }
+
+  addStaff() {
+      //
+  }
 }
--- src/providers/admin/admin.service.ts
+++ src/providers/admin/admin.service.ts
@@ -5,29 +5,30 @@ import { Student } from '../../modal/student';
 import { Observable, Subject } from 'rxjs';
 import { of } from 'rxjs';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class AdminService {
- students: Student[];
- public viewPanel = new Subject<boolean>();
+  students: Student[];
+  public viewPanel = new Subject<boolean>();
 
- constructor(public api: Api) {
+  constructor(public api: Api) {}
 
- }
+  getPrograms() {
+    return this.api.get('api/program/');
+  }
 
- getPrograms() {
-  return this.api.get('api/program/');
- }
+  getStudents(body: any) {
+    return this.api.post('api/student/filter', body);
+  }
 
- getStudents(body: any) {
-  return this.api.post('api/student/filter', body);
- }
+  getStudentById(studentId: number) {
+    return this.api.get('api/student/' + studentId);
+  }
 
- getStudentById(studentId: number) {
-  return this.api.get('api/student/' + studentId);
- }
+  deleteStudentById(studentId: number) {
+    return this.api.delete('api/student/' + studentId);
+  }
 
- deleteStudentById(studentId: number) {
-  return this.api.delete('api/student/' + studentId);
- }
-
+  getCenter() {
+    return this.api.get('api/center/');
+  }
 }
